d87c192df579f58760065e9615e610bedc628510
Aatheeriss <denissazonenko04@gmail.com>
2023-09-28 12:43:47 +0300

    quadEquation and lpr update

--- topic_02/quadEquation.py
+++ topic_02/quadEquation.py
@@ -13,16 +13,22 @@ D = dSearching(a, b, c)
 print("\nD =", D)
 
 #topic_02
-#D conditions check, roots calculating and output 
-if D > 0:
+#roots calculating function
+def calculatingRoots(b, D, a):
     x1 = (-b + D**0.5) / (2*a)
     x2 = (-b - D**0.5) / (2*a)
+    if x1 == x2:
+        return x1
+    else: 
+        return x1, x2
+    
+#D conditions check and roots output 
+if D > 0:
     print("\nEquation has 2 roots: ")
-    print("x1 =", x1)
-    print("x2 =", x2)
+    print("x =", calculatingRoots(b, D, a))   
 elif D == 0:
     x = -b / (2*a)
     print("\nEquation has 1 root: ")
-    print("x =", x)
+    print("x =", calculatingRoots(b, D, a))
 else:
     print("\nEquation has no roots.")
